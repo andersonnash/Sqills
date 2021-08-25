a83fd4f9882367410e86a9c9df9971421f14fe1b
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-08-25 15:16:05 -0600

    redo post

--- db/migrate/20210825203334_create_posts.rb
+++ db/migrate/20210825203334_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.text :image_url
       t.text :description
-
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
