adfb4f82beb00229d185748a7219a4b730cf384f
Nash Anderson <nashanderson@Nashs-Air.hsd1.tx.comcast.net>
2021-08-31 10:15:09 -0500

    some mobile views

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -10,24 +10,30 @@ export default function Layout(props) {
       </Route> */}
       
       {currentUser ? (
-        <div className="flex justify-between my-6">
-          <div className='flex justigy-center space-between mr-4'>
+        
+        <div className=" flex justify-between items-center my-6">
+        
+          <div className='flex space-between mr-4'>
+        
             <Link to='/posts'>
           <h1 className='text-xl font-bold ml-12 text-5xl'>SQILLS</h1>
             </Link>
           </div>
-          <p className='font-bold text-lg'> Welcome, {currentUser.username}!</p>
-            <button className='mr-4 text-lg font-bold' onClick={handleLogout}>Logout</button>
+          <p className='hidden md:block  animate-bounce font-bold text-lg'> Welcome, {currentUser.username}!</p>
+            <button className=' mr-4 text-lg font-bold ' onClick={handleLogout}>Logout</button>
             </div>
       ) : (
           <>
-          <Link to='/signup'>Register</Link>
-          <Link to='/Login'>Login</Link>
-        </>
+         {/* <div className='flex items-center'> */}
+          <h1 className='flex justify-center text-xl font-bold ml-12 text-5xl'>SQILLS</h1>
+          <Link className=" justify-end inline-block px-2 py-1 w-5/16 bg-gray-400 hover:bg-blue-600 p-4 opacity-50 rounded-lg text-lg text-gray-900  transition duration-300 m-6" to='/signup'>Register</Link>
+          <Link className=" inline-block px-4 py-1 w-5/16 bg-gray-400 hover:bg-blue-600 p-4 opacity-50 rounded-lg text-lg text-gray-900  transition duration-300 m-6" to='/Login'>Login</Link>
+        {/* </div> */}
+            </>
       )}
       {/* <hr /> */}
       {currentUser && (
-        <div>
+        <div className=''>
           <Link className=" inline-block px-2 py-1 w-1/3 bg-gray-400 hover:bg-blue-600 p-4 opacity-50 rounded-lg text-lg text-gray-900  transition duration-300 m-6" to='/users/1'>Your Activities</Link>
           <Link className=" inline-block px-2 py-1 w-1/3 bg-gray-400 hover:bg-blue-600 p-4 opacity-50 rounded-lg text-lg text-gray-900  transition duration-300" to='/categories'>All Categories</Link>
         </div>
