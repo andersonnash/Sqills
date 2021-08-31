f94cb92316e6d2deafc0c8968fec6bb7d9896252
Nash Anderson <nashanderson@Nashs-Air.hsd1.tx.comcast.net>
2021-08-31 11:27:18 -0500

    more cleaning

--- client/src/views/PostDetail/PostDetail.jsx
+++ client/src/views/PostDetail/PostDetail.jsx
@@ -6,7 +6,6 @@ import { Link } from "react-router-dom";
 export default function PostDetail(props) {
   const [postItem, setPostItem] = useState(null);
   const { id } = useParams();
-  // const { categories } = props;
 
   useEffect(() => {
     const fetchPostItem = async () => {
